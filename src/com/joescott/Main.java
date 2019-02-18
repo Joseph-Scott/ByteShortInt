1e3d789b2c1cc159eaf4d738517351865b44bf1a
Joseph Scott <jbs198585@gmail.com>
2019-02-18 12:56:20 -0500

    data types practice

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -4,22 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // int has a width of 32
-        int myMinValue = -2_147_483_648;
-        int myMaxValue = 2_147_483_647;
-        int myTotal = (myMinValue/2);
-        System.out.println("myTotal = " + myTotal);
+        // 1. Create a byte variable and set it to any valid byte number.
+        byte myByte = 10;
 
-        // byte has a width of 8
-        byte myByteValue = -128;
-        byte myNewByteValue = (byte) (myByteValue/2);
-        System.out.println("MyNewByteValue = " + myNewByteValue);
+        // 2. Create a short variable and set it to any valid short number.
+        short myShort = 20;
 
-        // short has a width of 16
-        short myShortValue = 32767;
-        short myNewShortValue = (short) (myShortValue / 2);
+        // 3. Create a int variable and set it to any valid int number.
+        int myInt = 50;
 
-        // long has a width of 64
-        long myLongValue = 100L;
+        // 4. Create a variable of type long, and make it equal to
+        // 50000 + 10 times the sum of the byte, plus the short plus the int
+        long longTotal = 50000 + 10 * (myByte + myShort + myInt);
+        short shortTotal = (short) (1000 + 10 * (myByte + myShort + myInt));
+        System.out.println("longTotal = " + longTotal);
+        System.out.println("shortTotal = " + shortTotal);
+
+
     }
 }
